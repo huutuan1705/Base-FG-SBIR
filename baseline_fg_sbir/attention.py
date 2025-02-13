8aa595ed785d104dad908e9e7f019f4d859add00
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-13 09:48:46 +0700

    update attention

--- baseline_fg_sbir/attention.py
+++ baseline_fg_sbir/attention.py
@@ -1,21 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import encoding
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class AttentionImage(nn.Module):
     def __init__(self, input_size=2048, output_size=64):
         super(AttentionImage, self).__init__()  
-        self.head_layer = nn.Linear(input_size, output_size)
+        self.head_layer = nn.Sequential(
+            encoding.nn.Normalize(),
+            nn.Linear(input_size, output_size),
+            encoding.nn.Normalize()
+        )
 
     def forward(self, x):
         x = F.adaptive_avg_pool2d(x, (1, 1))
         x = x.view(x.size(0), -1)
-        
-        x = F.normalize(x)
-        x = self.head_layer(x)
-        embedding = F.normalize(x)
+        embedding = self.head_layer(x)
         
         return embedding
     
