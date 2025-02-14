5d258ee866fcba33cf5277f045119ef1abd27e16
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-14 14:25:20 +0700

    update model

--- baseline_fg_sbir/model.py
+++ baseline_fg_sbir/model.py
@@ -40,8 +40,8 @@ class FGSBIR_Model(nn.Module):
         
     def test_forward(self, batch):
         # sketch_feature = self.sketch_embedding_network(batch['sketch_img'].to(device))
-        sketch_feature = self.sample_train_params(batch['sketch_img'].to(device))
-        positive_feature = self.sample_train_params(batch['positive_img'].to(device))
+        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
+        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
         
         if self.args.use_attention:
             positive_feature = self.attention(positive_feature)
@@ -57,9 +57,9 @@ class FGSBIR_Model(nn.Module):
         self.train()
         self.optimizer.zero_grad()
             
-        positive_feature = self.sample_train_params(batch['positive_img'].to(device), 1)
-        negative_feature = self.sample_train_params(batch['negative_img'].to(device), 1)
-        sketch_feature = self.sample_train_params(batch['sketch_img'].to(device), 1)
+        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device), 1)
+        negative_feature = self.sample_embedding_network(batch['negative_img'].to(device), 1)
+        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device), 1)
         
         if self.args.use_attention:
             positive_feature = self.attention(positive_feature)
