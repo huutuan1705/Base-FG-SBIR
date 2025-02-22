1b32c7deeb7d6e83d01d2dcd59adf70219e66a28
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-22 16:47:32 +0700

    convert to old idea

--- baseline_fg_sbir/backbones.py
+++ baseline_fg_sbir/backbones.py
@@ -101,10 +101,10 @@ class InceptionV3(nn.Module):
         # N x 2048 x 8 x 8
         x = self.Mixed_7c(x)
 
-        feature = self.pool_method(x).view(-1, 2048)
-        return F.normalize(feature)
+        # feature = self.pool_method(x).view(-1, 2048)
+        # return F.normalize(feature)
 
-        # return x
+        return x
         
     def fix_weights(self):
         for x in self.parameters():
