39d628d1c27f62540d9ee7d26ac81c9cc96614a8
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-22 21:36:27 +0700

    convert to old idea

--- baseline_fg_sbir/model.py
+++ baseline_fg_sbir/model.py
@@ -41,9 +41,9 @@ class FGSBIR_Model(nn.Module):
         negative_feature = self.sample_embedding_network(batch['negative_img'].to(device))
         sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
         
-        positive_feature = self.attention(positive_feature)
-        negative_feature = self.attention(negative_feature)
-        sketch_feature = self.attention(sketch_feature)
+        # positive_feature = self.attention(positive_feature)
+        # negative_feature = self.attention(negative_feature)
+        # sketch_feature = self.attention(sketch_feature)
         
         # positive_feature = self.linear(self.attention(positive_feature))
         # negative_feature = self.linear(self.attention(negative_feature))
@@ -59,8 +59,8 @@ class FGSBIR_Model(nn.Module):
         positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
         sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
         
-        positive_feature = self.attention(positive_feature)
-        sketch_feature = self.attention(sketch_feature)
+        # positive_feature = self.attention(positive_feature)
+        # sketch_feature = self.attention(sketch_feature)
         # positive_feature = self.linear(self.attention(positive_feature))
         # sketch_feature = self.sketch_linear(self.sketch_attention(sketch_feature))
         
