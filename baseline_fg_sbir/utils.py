4ebfa13230f22989af7633aa2cdc0843691419de
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-23 22:23:33 +0700

    update model

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -17,6 +17,9 @@ def get_transform(type):
             transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),  # Dịch chuyển ảnh
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Điều chỉnh màu sắc
             transforms.GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),  # Làm mờ ảnh ngẫu nhiên
+            transforms.Lambda(lambda x: x + 0.01 * torch.randn_like(x)),  # Thêm nhiễu ngẫu nhiên
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
         
     return transforms.Compose(transform_list)
