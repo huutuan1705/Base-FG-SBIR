ea8ac1dd5b9ca4755cc337f15cdf54afcdbbaee2
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-23 08:59:39 +0700

    convert to old idea

--- baseline_fg_sbir/dataset.py
+++ baseline_fg_sbir/dataset.py
@@ -1,108 +1,93 @@
-import torch
-import pickle
-import torch.utils.data as data
-import torchvision.transforms as transforms
 import os
-from random import randint
-from PIL import Image
+import pickle
+import torch
 import random
 import torchvision.transforms.functional as F
-from rasterize import rasterize_Sketch
+
+from torch.utils.data import Dataset
+from utils import get_transform
+from random import randint
+from PIL import Image
+
+from rasterize import rasterize_sketch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class FGSBIR_Dataset(data.Dataset):
-    def __init__(self, args, mode):
-
-        self.args = args
+class FGSBIR_Dataset(Dataset):
+    def __init__(self, hp, mode):
+        self.hp = hp
         self.mode = mode
-        coordinate_path = os.path.join(args.root_dir, args.dataset_name , args.dataset_name + '_Coordinate')
-        self.root_dir = os.path.join(args.root_dir, args.dataset_name)
-        with open(coordinate_path, 'rb') as fp:
-            self.Coordinate = pickle.load(fp)
-
-        self.Train_Sketch = [x for x in self.Coordinate if 'train' in x]
-        self.Test_Sketch = [x for x in self.Coordinate if 'test' in x]
-
-        self.train_transform = get_ransform('Train')
-        self.test_transform = get_ransform('Test')
-
-    def __getitem__(self, item):
-        sample  = {}
-        if self.mode == 'Train':
-            sketch_path = self.Train_Sketch[item]
-
-            positive_sample = '_'.join(self.Train_Sketch[item].split('/')[-1].split('_')[:-1])
-            positive_path = os.path.join(self.root_dir, 'photo', positive_sample + '.png')
-
-            possible_list = list(range(len(self.Train_Sketch)))
-            possible_list.remove(item)
-            negative_item = possible_list[randint(0, len(possible_list) - 1)]
-            negative_sample = '_'.join(self.Train_Sketch[negative_item].split('/')[-1].split('_')[:-1])
-            negative_path = os.path.join(self.root_dir, 'photo', negative_sample + '.png')
-
-            vector_x = self.Coordinate[sketch_path]
-            sketch_img = rasterize_Sketch(vector_x)
-            sketch_img = Image.fromarray(sketch_img).convert('RGB')
-
-            positive_img = Image.open(positive_path).convert('RGB')
-            negative_img = Image.open(negative_path).convert('RGB')
-
-            n_flip = random.random()
-            if n_flip > 0.5:
-                sketch_img = F.hflip(sketch_img)
-                positive_img = F.hflip(positive_img)
-                negative_img = F.hflip(negative_img)
-
-            sketch_img = self.train_transform(sketch_img)
-            positive_img = self.train_transform(positive_img)
-            negative_img = self.train_transform(negative_img)
-
-            sample = {'sketch_img': sketch_img, 'sketch_path': sketch_path,
-                      'positive_img': positive_img, 'positive_path': positive_sample,
-                      'negative_img': negative_img, 'negative_path': negative_sample
-                      }
-
-        elif self.mode == 'Test':
-
-            sketch_path = self.Test_Sketch[item]
-            vector_x = self.Coordinate[sketch_path]
-            sketch_img = rasterize_Sketch(vector_x)
-            sketch_img = self.test_transform(Image.fromarray(sketch_img).convert('RGB'))
-
-            positive_sample = '_'.join(self.Test_Sketch[item].split('/')[-1].split('_')[:-1])
-            positive_path = os.path.join(self.root_dir, 'photo', positive_sample + '.png')
-            positive_img = self.test_transform(Image.open(positive_path).convert('RGB'))
-
-            sample = {'sketch_img': sketch_img, 'sketch_path': sketch_path, 'Coordinate':vector_x,
-                      'positive_img': positive_img, 'positive_path': positive_sample}
-
-        return sample
-
+        
+        coordinate_path = os.path.join(hp.root_dir, hp.dataset_name, hp.dataset_name + '_Coordinate')
+        self.root_dir = os.path.join(hp.root_dir, hp.dataset_name)
+        with open(coordinate_path, 'rb') as f:
+            self.coordinate = pickle.load(f)
+            
+        self.train_sketch = [x for x in self.coordinate if 'train' in x]
+        self.test_sketch = [x for x in self.coordinate if 'test' in x]
+        
+        self.train_transform = get_transform('train')
+        self.test_transform = get_transform('test')
+        
     def __len__(self):
-        if self.mode == 'Train':
-            return len(self.Train_Sketch)
-        elif self.mode == 'Test':
-            return len(self.Test_Sketch)
+        if self.mode == 'train':
+            return len(self.train_sketch)
 
-def get_dataloader(args):
-
-    dataset_Train  = FGSBIR_Dataset(args, mode = 'Train')
-    dataloader_Train = data.DataLoader(dataset_Train, batch_size=args.batchsize, shuffle=True,
-                                         num_workers=int(args.nThreads))
-
-    dataset_Test  = FGSBIR_Dataset(args, mode = 'Test')
-    dataloader_Test = data.DataLoader(dataset_Test, batch_size=1, shuffle=False,
-                                         num_workers=int(args.nThreads))
-
-    return dataloader_Train, dataloader_Test
-
-def get_ransform(type):
-    transform_list = []
-    if type == 'Train':
-        transform_list.extend([transforms.Resize(299)])
-    elif type == 'Test':
-        transform_list.extend([transforms.Resize(299)])
-    transform_list.extend(
-        [transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    return transforms.Compose(transform_list)
+        return len(self.test_sketch)
+        
+        
+    def __getitem__(self, item):
+        sample = {}
+        
+        if self.mode == 'train':
+            sketch_path = self.train_sketch[item]
+            
+            positive_sample = '_'.join(self.train_sketch[item].split('/')[-1].split('_')[:-1])
+            positive_path = os.path.join(self.root_dir, 'photo', positive_sample + '.png')
+            
+            posible_list = list(range(len(self.train_sketch)))
+            posible_list.remove(item)
+            
+            negative_item = posible_list[randint(0, len(posible_list)-1)]
+            negative_sample = '_'.join(self.train_sketch[negative_item].split('/')[-1].split('_')[:-1])
+            negative_path = os.path.join(self.root_dir, 'photo', negative_sample + '.png')
+            
+            vector_x = self.coordinate[sketch_path]
+            sketch_img = rasterize_sketch(vector_x)
+            # <class 'numpy.ndarray'>
+            # print("Shape:", sketch_img.shape, "Dtype:", sketch_img.dtype)
+               
+            sketch_img = Image.fromarray(sketch_img).convert("RGB")
+            
+            positive_image = Image.open(positive_path).convert("RGB")
+            negative_image = Image.open(negative_path).convert("RGB")
+            
+            # n_flip = random.random()
+            # if n_flip > 0.5:
+            #     sketch_img = F.hflip(sketch_img)
+            #     positive_image = F.hflip(positive_image)
+            #     negative_image = F.hflip(negative_image)
+            
+            sketch_img = self.train_transform(sketch_img)
+            positive_image = self.train_transform(positive_image)
+            negative_image = self.train_transform(negative_image)
+            
+            sample = {'sketch_img': sketch_img, 'sketch_path': sketch_path,
+                      'positive_img': positive_image, 'positive_path': positive_sample,
+                      'negative_img': negative_image, 'negative_path': negative_sample
+                      } 
+        
+        elif self.mode == "test":
+            sketch_path = self.test_sketch[item] 
+            vector_x = self.coordinate[sketch_path]
+            sketch_img = rasterize_sketch(vector_x)
+            sketch_img = self.test_transform(Image.fromarray(sketch_img).convert("RGB"))
+            
+            positive_sample = '_'.join(self.test_sketch[item].split('/')[-1].split('_')[:-1])
+            positive_path = os.path.join(self.root_dir, 'photo', positive_sample + '.png')
+            positive_image = self.test_transform(Image.open(positive_path).convert("RGB"))
+            
+            sample = {'sketch_img': sketch_img, 'sketch_path': sketch_path, 'Coordinate':vector_x,
+                      'positive_img': positive_image, 'positive_path': positive_sample}
+            
+        return sample
