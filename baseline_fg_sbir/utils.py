1de56b5b21c9515b23d6fc47ca53cc2e984192ef
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-04 10:06:05 +0700

    update model and attention

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -9,7 +9,8 @@ def get_transform(type):
     
     if type == 'train':
         transform_list.extend([
-            transforms.RandomHorizontalFlip(0.4),
+            transforms.RandomHorizontalFlip(0.25),
+            transforms.RandomVerticalFlip(0.25),
             transforms.RandomRotation(15),
         ])
         
