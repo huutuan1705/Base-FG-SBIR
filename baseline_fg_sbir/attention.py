62c5a06d48763e751bdce3d8bc4c1cc539466efa
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-13 11:06:00 +0700

    update attention

--- baseline_fg_sbir/attention.py
+++ baseline_fg_sbir/attention.py
@@ -29,8 +29,8 @@ class SelfAttention(nn.Module):
         super(SelfAttention, self).__init__()
         self.pool_method =  nn.AdaptiveMaxPool2d(1) # as default
         self.norm = nn.LayerNorm(2048)
-        # self.mha = nn.MultiheadAttention(2048, num_heads=args.num_heads, batch_first=True)
-        self.mha = nn.MultiheadAttention(2048, num_heads=8, batch_first=True)
+        self.mha = nn.MultiheadAttention(2048, num_heads=args.num_heads, batch_first=True)
+        # self.mha = nn.MultiheadAttention(2048, num_heads=8, batch_first=True)
         self.scale = nn.Parameter(torch.zeros(1))
         
     def forward(self, x):
@@ -54,8 +54,8 @@ class Linear_global(nn.Module):
     def forward(self, x):
         return F.normalize(self.head_layer(x))
     
-input_tensor = torch.randn(68, 2048, 8, 8)
-model = SelfAttention(None)
-output = model(input_tensor)
+# input_tensor = torch.randn(68, 2048, 8, 8)
+# model = SelfAttention(None)
+# output = model(input_tensor)
 
-print("Output shape:", output.shape)
+# print("Output shape:", output.shape)
