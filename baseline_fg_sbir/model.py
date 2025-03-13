dc0f5292b1c4bc22899f84ef832afed30724f02c
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-13 08:12:02 +0700

    update attention

--- baseline_fg_sbir/model.py
+++ baseline_fg_sbir/model.py
@@ -31,8 +31,8 @@ class FGSBIR_Model(nn.Module):
         self.linear_params = self.linear.parameters()
         
         self.sketch_embedding_network = eval(args.backbone_name + "(args)")
-        self.sketch_attention = Attention_global()
-        # self.sketch_attention = SelfAttention(args)
+        # self.sketch_attention = Attention_global()
+        self.sketch_attention = SelfAttention(args)
         self.sketch_linear = Linear_global(feature_num=self.args.output_size)
         
 
@@ -52,17 +52,17 @@ class FGSBIR_Model(nn.Module):
             
         positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
         negative_feature = self.sample_embedding_network(batch['negative_img'].to(device))
-        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
+        sketch_feature = self.sketch_embedding_network(batch['sketch_img'].to(device))
         
         if self.args.use_attention:
             positive_feature = self.attention(positive_feature)
             negative_feature = self.attention(negative_feature)
-            sketch_feature = self.attention(sketch_feature)
+            sketch_feature = self.sketch_attention(sketch_feature)
             
         if self.args.use_linear:
             positive_feature = self.linear(positive_feature)
             negative_feature = self.linear(negative_feature)
-            sketch_feature = self.linear(sketch_feature)
+            sketch_feature = self.sketch_linear(sketch_feature)
 
         loss = self.loss(sketch_feature, positive_feature, negative_feature)
         loss.backward()
@@ -71,16 +71,16 @@ class FGSBIR_Model(nn.Module):
         return loss.item() 
 
     def test_forward(self, batch):
-        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
+        sketch_feature = self.sketch_embedding_network(batch['sketch_img'].to(device))
         positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
         
         if self.args.use_attention:
             positive_feature = self.attention(positive_feature)
-            sketch_feature = self.attention(sketch_feature)
+            sketch_feature = self.sketch_attention(sketch_feature)
         
         if self.args.use_linear:
             positive_feature = self.linear(positive_feature)
-            sketch_feature = self.linear(sketch_feature)
+            sketch_feature = self.sketch_linear(sketch_feature)
             
         return sketch_feature, positive_feature
     
