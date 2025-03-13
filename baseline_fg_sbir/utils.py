b747807d859ae8f5b22d39c04cb0b148f6fad348
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-13 08:00:23 +0700

    update attention

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -5,7 +5,6 @@ def get_transform(type):
         transform_list = [
             transforms.Resize(299),
             transforms.RandomHorizontalFlip(0.5),
-            transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15),
             transforms.RandomRotation(15),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
