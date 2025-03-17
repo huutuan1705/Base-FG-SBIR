bbe3f5b51a5b11c05879277288e7230a4e01badd
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-17 11:03:48 +0700

    update main

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -4,10 +4,9 @@ def get_transform(type):
     if type == 'train':
         transform_list = [
             transforms.Resize(299),
-            transforms.RandomHorizontalFlip(0.3),
-            transforms.RandomVerticalFlip(0.3),
-            transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3),
-            transforms.RandomRotation(30),
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15),
+            transforms.RandomRotation(15),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
