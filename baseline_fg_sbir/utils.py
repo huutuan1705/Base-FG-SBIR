70dbbd552f4af3308ef31675da146d5df86ee7ed
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-19 10:48:39 +0700

    update dataset

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -4,8 +4,8 @@ def get_transform(type):
     if type == 'train':
         transform_list = [
             transforms.Resize(299),
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.RandomRotation(20),
+            # transforms.RandomHorizontalFlip(0.5),
+            # transforms.RandomRotation(20),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
